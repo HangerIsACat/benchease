698d5d21fad975daf1ce9e0297928147ca28b749
sheenam <sheenam@answerhub.com>
2023-04-11 17:19:51 +0800

    Update codev.synergy.controller.SampleAPI.java to add mapping for helloWorld()

--- src/main/java/codev/synergy/controller/SampleAPI.java
+++ src/main/java/codev/synergy/controller/SampleAPI.java
@@ -1,10 +1,14 @@
 package codev.synergy.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("sample")
 public class SampleAPI {
 
-  @RequestMapping("/")
+  @GetMapping(path = "/hello")
   public String helloWorld() {
     return "Hello, World!"; 
   }
