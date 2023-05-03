b88934f3ae87f6a08d663703c7544b11dc319030
sheenam <sheenam@answerhub.com>
2023-05-03 18:24:22 +0800

    Update HotSkillController.getHotSkills() to process skills in SkillService

--- src/main/java/codev/synergy/controllers/HotSkillController.java
+++ src/main/java/codev/synergy/controllers/HotSkillController.java
@@ -1,13 +1,14 @@
 package codev.synergy.controllers;
 
 import codev.synergy.entities.Skill;
+import codev.synergy.services.SkillService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,26 +16,13 @@ import java.util.Optional;
 @RequestMapping("/benchease/v1/hotskills")
 public class HotSkillController {
 
-    private int DEFAULT_HOT_SKILL_COUNT = 10;
+    @Autowired
+    private SkillService serviceSkill;
 
     @GetMapping
     public ResponseEntity<List<Skill>> getHotSkills(@RequestParam Optional<Integer> count) {
-
-        List<Skill> skills = new ArrayList<>();
-
-        int noOfSkills = count.isPresent() ? count.get() : DEFAULT_HOT_SKILL_COUNT;
-        for (int i = 0; i < noOfSkills; i++) {
-            Skill skill = new Skill( (long) i + 1, "Skill " + (i + 1) );
-            skill.incrementSearchCount();
-            skill.incrementSearchCount();
-            skill.incrementSearchCount();
-            skill.setApproved(true);
-
-            skills.add(skill);
-        }
-
-        return ResponseEntity.ok(skills);
-
+        int noOfSkills = count.isPresent() ? count.get() : SkillService.DEFAULT_HOT_SKILL_COUNT;
+        return ResponseEntity.ok(serviceSkill.getTopSkills(noOfSkills));
     }
 
 }
