1c1de689f728e9426753569a654662eb7e151f49
sheenam <sheenam@answerhub.com>
2023-05-06 01:44:47 +0800

    Add category property, its getter and setter methods, new constructors

--- src/main/java/codev/synergy/entities/EmployeeSkill.java
+++ src/main/java/codev/synergy/entities/EmployeeSkill.java
@@ -1,20 +1,39 @@
 package codev.synergy.entities;
 
 
+import java.util.Date;
+
 public class EmployeeSkill extends Skill {
-    private String type;
+    private String category;
 
-    public EmployeeSkill(String id, String title, String type) {
+    public EmployeeSkill(String title) {
+        super(title);
+    }
+    public EmployeeSkill(String id, String title, String category) {
         super(id, title);
-        this.setType(type);
+        this.setCategory(category);
     }
 
-    public String getType() {
-        return type;
+    public EmployeeSkill(
+        String id,
+        String title,
+        boolean isApproved,
+        String category,
+        Date createdAt,
+        String createdBy,
+        Date updatedAt,
+        String updatedBy) {
+
+        super(id, title, 0, isApproved, createdAt, createdBy, updatedAt, updatedBy);
+        this.setCategory(category);
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
 }
