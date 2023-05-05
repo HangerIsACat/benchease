b949f481990dac20b86578f30d4147aaf6a31293
sheenam <sheenam@answerhub.com>
2023-05-06 01:46:20 +0800

    Add new constructors

--- src/main/java/codev/synergy/entities/BenchEmployee.java
+++ src/main/java/codev/synergy/entities/BenchEmployee.java
@@ -7,6 +7,19 @@ public class BenchEmployee extends Employee {
     private List<EmployeeSkill> skills;
 
     public BenchEmployee(
+            String id,
+            String empId,
+            String firstName,
+            String middleName,
+            String lastName,
+            String email,
+            int status,
+            boolean isNew) {
+
+        super(id, empId, firstName, middleName, lastName, email, status, isNew);
+    }
+    public BenchEmployee(
+            String id,
             String empId,
             String firstName,
             String middleName,
@@ -16,8 +29,21 @@ public class BenchEmployee extends Employee {
             boolean isNew,
             List<EmployeeSkill> skills) {
 
-        super(empId, firstName, middleName, lastName, email, status, isNew);
+        super(id, empId, firstName, middleName, lastName, email, status, isNew);
         this.setSkills(skills);
+    }
+
+    public BenchEmployee(Employee employee, List<EmployeeSkill> skills) {
+        this(
+            employee.getId(),
+            employee.getEmpId(),
+            employee.getFirstName(),
+            employee.getMiddleName(),
+            employee.getLastName(),
+            employee.getEmail(),
+            employee.getStatus(),
+            employee.isNew(),
+            skills);
     }
 
     public List<EmployeeSkill> getSkills() {
